3992addc46ab91c00e502e13f89a78e0ba1dba05
Nurlan <nurlanea@code.edu.az>
2023-10-31 23:56:25 +0400

    upload

---
+++ 1den20ye/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(num(new int [] { 1, 3, 55, 6, 33}));
+
+int num (int[] n)
+{int product = 1;
+	foreach (var i in n)
+	{
+		if ((i >= 1) && (i <= 20))
+		{
+            product *= i;
+        }
+    }
+	return product;
+}
+
